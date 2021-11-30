0fe4584c14ef0c0bc827c0f64ff3fcdd65c11fb8
rodolfoHOk <rodolfo.okino@gmail.com>
2021-11-30 01:50:01 -0300

    Acessando propriedades com @ConfigurationProperties

---
+++ src/main/java/com/algaworks/spring/notificacao/NotificadorProperties.java
@@ -0,0 +1,32 @@
+package com.algaworks.spring.notificacao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email")
+public class NotificadorProperties {
+	
+	/**
+	 * Host do servidor
+	 */
+	private String hostServidor;
+	
+	/**
+	 * Porta do servidor
+	 */
+	private Integer portaServidor = 25;
+	
+	public String getHostServidor() {
+		return hostServidor;
+	}
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
+}
