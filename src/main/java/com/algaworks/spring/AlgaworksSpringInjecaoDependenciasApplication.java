95e33b60d2d7a3d9872fc2bf8da6676ad194a5b7
rodolfoHOk <rodolfo.okino@gmail.com>
2021-11-29 23:46:22 -0300

    Criando um projeto Spring Boot com Spring Initializr

---
+++ src/main/java/com/algaworks/spring/AlgaworksSpringInjecaoDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksSpringInjecaoDependenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksSpringInjecaoDependenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/spring/AlgaworksSpringInjecaoDependenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksSpringInjecaoDependenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
